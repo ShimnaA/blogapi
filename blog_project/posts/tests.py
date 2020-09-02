65ca0d8b619fd4920bd5d7e67728170841d5af4f
shimna <shimnaa.arangil@gmail.com>
2020-09-02 16:45:22 +0530

    test case for checking getallposts

--- blog_project/posts/tests.py
+++ blog_project/posts/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your tests here.
+
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    @staticmethod
+    def create_post(title="", content=""):
+        if title != "" and content != "":
+            Post.objects.create(title=title, content=content)
+
+    def setUp(self):
+        #add test data
+        self.create_post("title1", "content1")
+        self.create_post("title2", "content2")
+        self.create_post("title3", "content3")
+
+class GetAllPostsTest(BaseViewTest):
+
+    def test_get_all_posts(self):
+        """
+        This test ensures that all posts added in the setUp method
+        exist when we make a GET request to the songs/ endpoint
+        """
+        # hit the API endpoint
+        response = self.client.get(reverse("all-posts"))
+        # fetch the data from db
+        expected = Post.objects.all()
+        print(expected)
+        serialized = PostSerializer(expected, many=True)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
--- blog_project/posts/urls.py
+++ blog_project/posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
+    path('', views.PostList.as_view(),name="all-posts"),
     path('<int:pk>/', views.PostDetail.as_view()),
 ]
